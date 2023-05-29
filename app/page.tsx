acb42a56967212946ff95a08750cdeac9af056e8
Daniel Ukaji <dannyukaji@gmail.com>
2023-05-29 13:01:47 +0100

    New Commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Header from './components/Header'
+import Board from './components/Board'
+
+export default function Home() {
+  return (
+    <main >
+      {/* Header */}
+      <Header />
+      {/* Board */}
+      <Board />
+      <h1>Trello 2.0 Clone</h1>
+    </main>
+  )
+}
